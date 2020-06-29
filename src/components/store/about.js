b325e560b2f242ed80081dc7efa234d75f19c989
Hudson Havenhill <havenhillhudson@gmail.com>
2020-06-29 15:33:50 -0400

    Colors

--- src/components/store/about.js
+++ src/components/store/about.js
@@ -6,7 +6,7 @@ module.exports = {
             id: 0,
             snippet: {
                 title: 'LiveInColor',
-                color: '#999999',
+                color: '#737373',
                 position: 'Independent Media Platform',
                 positionDescription: '',
                 description: 'LiveInColor is designed to be an independent media platform where we bring a variety of awesome things to you at home such as music, cooking tutorials, stories, reviews, and traveling. Starting with brand new music from Charles Hussle, an up and coming rapper/writer/singer-song-writer, you will surely not be disappointed. Additionally, LiveInColor features very simple cooking tutorials taught by Chef BoyAD, reviews on various topics and traveling vlogs to some of the most beautiful sights in the world. Aside from creating content, at LiveInColor we love to volunteer and help with charity. Since 2019 we have raised close to $5000 for a number of organizations including PALS, which is an organization that creates programs for young adults with down syndrome, the Children’s Miracle Network, and the Boys and Girls club of America. With the help of our new followers we hope to double that fundraising amount  in the coming year and continue to spread happiness, positivity, knowledge and quality music.',
@@ -19,7 +19,7 @@ module.exports = {
             id: 0,
             snippet: {
                 title: 'Aditya Sharma',
-                color: '#ff748c',
+                color: '#ad009f',
                 position: 'Co-founder, LiveInColor',
                 positionDescription: 'Content Creator, Editor, Labelowner, Manager, Marketing, Finances, Merchandising',
                 description: 'AD has always had a passion for creating goofy videos, but in college with the help of his roommate Hudson, LiveInColor was able to be created. Since starting with videos,  LiveInColor has grown multidimensionally as a brand. Aside from creating videos, AD also owns and manages the label, as well as handling all of the organization`s finances, promotional growth, and merchandising. Of course, none of this would be possible without the help of the extremely dedicated hardworking individuals who comprise the LiveInColor Team. In the future, AD hopes to expose new people to the joy that LiveInColor brings while continuing our mission to give back to the community.',
@@ -32,7 +32,7 @@ module.exports = {
             id: 0,
             snippet: {
                 title: 'Hudson Havenhill',
-                color: '#2266ff',
+                color: '#5700ad',
                 position: 'Co-founder, LiveInColor',
                 positionDescription: 'Video editing and production, graphic design, music production, photography, website design',
                 description: 'As part of LiveInColor, Hudson handles much of the behind-the-scenes work in video editing and production, as well in designing LiveInColor’s aesthetic. He also makes the music heard in many of our videos, and takes a lot of the photos on our Instagram. In his free time he loves playing video games, reading, walking in nature, and watching lectures. He hopes to expand his music and video making experience through future LiveInColor projects, and collaborating with talented creators. He is currently pursuing a Bachelor’s degree in History and Political Science at the University of Pittsburgh.',
@@ -45,7 +45,7 @@ module.exports = {
             id: 0,
             snippet: {
                 title: 'Charles Hussle',
-                color: '#2266ff',
+                color: '#1a00ad',
                 position: 'Artist',
                 positionDescription: 'Rapper, songwriter',
                 description: 'Jonathan Ray, more popularly known as Charles Hussle, is an up and coming rapper, writer, poet, and director hailing from the city of Geneva, New York. He is a senior at the University of Pittsburgh, majoring in English Writing with a minor in Creative Non-Fiction. His inspirations range from Coltrane to Q-Tip, and anything in between.',
@@ -58,7 +58,7 @@ module.exports = {
             id: 0,
             snippet: {
                 title: 'Prit Desai',
-                color: '#dbba00',
+                color: '#008aad',
                 position: 'Fundraising Manager',
                 positionDescription: '',
                 description: 'To date, Prit has helped us raise close to $5000 for charitable organizations such as PALS, Children’s Miracle Network, and the Boys and Girls Club of America. Prit has helped LiveInColor do this in a variety of ways such as selling scrunchies, donuts, wristbands, stickers, shirts and matching donations. Prit is studying to be a healthcare professional and has a passion for helping people which has helped fuel his endeavors with LiveInColor. In the future, Prit hopes to become a physician’s assistant and also continue his work with LiveInColor as it is our mission to give back to the community in any way we can such as helping raise money, volunteering, or promoting awareness. ',
@@ -72,7 +72,7 @@ module.exports = {
             id: 0,
             snippet: {
                 title: 'Huw Messie',
-                color: '#f20041',
+                color: '#00ad65',
                 position: 'Designer',
                 positionDescription: 'Artist, Video Producer, Clothing Design',
                 description: 'Huw is a multimedia artist working in video/animation, game development and textiles. He’s produced several music videos in collaboration with LiveInColor and Charles Hussle, and works with LIC members on team projects. He also works experimentally in clothing and accessory design, using repurposed materials to create limited-edition pieces. Huw is a senior at Carnegie Mellon University studying fine arts and human-computer interaction.',
@@ -85,7 +85,7 @@ module.exports = {
             id: 0,
             snippet: {
                 title: 'Greyson Flippo',
-                color: '#f27500',
+                color: '#aecf08',
                 position: 'Designer',
                 positionDescription: 'Website Design, Programming, IT support',
                 description: 'Greyson is a Chicago-based website designer who is responsible for programming the LiveInColor website, and for assisting in the various technology that makes LiveInColor possible. He also is the designer of the Music Visualizer for Google Play™ Music, available for Google Chrome on the Chrome Web Store.',
@@ -98,7 +98,7 @@ module.exports = {
             id: 0,
             snippet: {
                 title: 'Raam Chinnaya',
-                color: '#0fb500',
+                color: '#cf9a08',
                 position: 'Designer',
                 positionDescription: 'Artist, Graphic Designer',
                 description: 'What’s up guys, I’m Raam and I have had the opportunity to come up with a whole bunch of designs surrounding the LIC brand. I hope to finalize them and see some designs released real soon. Other than that I am going to be a rising senior at the University of Pittsburgh, where I study Civil Engineering. Drawing and designing is a passion of mine so I am glad that through Live in Color I am able to still hold onto it after all of these years. Hope to update you all soon, peace.',
@@ -111,7 +111,7 @@ module.exports = {
             id: 0,
             snippet: {
                 title: 'Lorenzo Bucchalter',
-                color: '#0fb500',
+                color: '#cf3008',
                 position: 'Designer',
                 positionDescription: 'Artist, Graphic Designer, Video Production, Clothing Design, Painting',
                 description: 'Lorenzo has worked closely with LiveInColor since its inception, and produced and assited on multiple Charles Hussle music videos. He also designs clothing, and creates experimental and unique paintings. He is a recent graduate from University of Pittsburgh`s Art program.',
