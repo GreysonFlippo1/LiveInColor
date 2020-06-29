4e1ce36861a1bdad1387118ef717592fb4c9dbf7
Hudson Havenhill <havenhillhudson@gmail.com>
2020-06-29 14:36:16 -0400

    Update about.js

--- src/components/store/about.js
+++ src/components/store/about.js
@@ -5,11 +5,11 @@ module.exports = {
         {
             id: 0,
             snippet: {
-                title: 'Hudson Havenhill',
-                color: '#2266ff',
+                title: 'Aditya Sharma',
+                color: '#ff748c',
                 position: 'Co-founder, LiveInColor',
-                positionDescription: 'Video editing and production, graphic design, music production, photography.',
-                description: 'As part of LiveInColor, I handle much of the behind the scenes work in video editing and production. I also make the music heard in many of our videos, and take a lot of the photos on our instagram. In my free time I love playing video games, reading, and walking in nature. I am currently pursuing a Bachelor’s degree in History and Political Science at the University of Pittsburgh.',
+                positionDescription: 'Content Creator, Editor, Labelowner, Manager, Marketing, Finances, Merchandising',
+                description: 'AD has always had a passion for creating goofy videos, but in college with the help of his roommate Hudson, LiveInColor was able to be created. Since starting with videos,  LiveInColor has grown multidimensionally as a brand. Aside from creating videos, AD also owns and manages the label, as well as handling all of the organization`s finances, promotional growth, and merchandising. Of course, none of this would be possible without the help of the extremely dedicated hardworking individuals who comprise the LiveInColor Team. In the future, AD hopes to expose new people to the joy that LiveInColor brings while continuing our mission to give back to the community.',
                 images: [
                     'https://upload.wikimedia.org/wikipedia/commons/4/48/Alex_Jones_Portrait_%28cropped%29.jpg'
                     ],
@@ -18,15 +18,67 @@ module.exports = {
         {
             id: 0,
             snippet: {
-                title: 'Pink Guy',
-                color: '#ff748c',
-                position: 'Peace Lord',
-                positionDescription: 'Hey boss I have a cancer.',
-                description: 'Pink Guy entered Frank`s life when Frank`s original dimension in Okinawa disappeared, as Frank was in the realm of the darkness, Pink Guy appeared before him as a messenger of an unknown entity and tells him all about the Peace Lords and Chin-Chin, This was revealed in the book, Francis of the Filth, Pink Guy could not escape that realm.',
+                title: 'Hudson Havenhill',
+                color: '#2266ff',
+                position: 'Co-founder, LiveInColor',
+                positionDescription: 'Video editing and production, graphic design, music production, photography, website design',
+                description: 'As part of LiveInColor, Hudson handles much of the behind-the-scenes work in video editing and production, as well in designing LiveInColor’s aesthetic. He also makes the music heard in many of our videos, and takes a lot of the photos on our Instagram. In his free time he loves playing video games, reading, walking in nature, and watching lectures. He hopes to expand his music and video making experience through future LiveInColor projects, and collaborating with talented creators. He is currently pursuing a Bachelor’s degree in History and Political Science at the University of Pittsburgh.',
                 images: [
                     'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/33/33325298be9df72ed33c86775652dc94572fa248_full.jpg'
                     ],
             }
-        }
+        },
+		{
+            id: 0,
+            snippet: {
+                title: 'Prit Desai',
+                color: '#ff748c',
+                position: 'Fundraising Manager',
+                positionDescription: '',
+                description: 'To date, Prit has helped us raise close to $5000 for charitable organizations such as PALS, Children’s Miracle Network, and the Boys and Girls Club of America. Prit has helped LiveInColor do this in a variety of ways such as selling scrunchies, donuts, wristbands, stickers, shirts and matching donations. Prit is studying to be a healthcare professional and has a passion for helping people which has helped fuel his endeavors with LiveInColor. In the future, Prit hopes to become a physician’s assistant and also continue his work with LiveInColor as it is our mission to give back to the community in any way we can such as helping raise money, volunteering, or promoting awareness. ',
+                images: [
+                    'https://upload.wikimedia.org/wikipedia/commons/4/48/Alex_Jones_Portrait_%28cropped%29.jpg'
+                    ],
+            }
+        },
+		{
+            id: 0,
+            snippet: {
+                title: 'Raam Chinnaya',
+                color: '#ff748c',
+                position: 'Designer',
+                positionDescription: 'Artist, Graphic Designer',
+                description: 'What’s up guys, I’m Raam and I have had the opportunity to come up with a whole bunch of designs surrounding the LIC brand. I hope to finalize them and see some designs released real soon. Other than that I am going to be a rising senior at the University of Pittsburgh, where I study Civil Engineering. Drawing and designing is a passion of mine so I am glad that through Live in Color I am able to still hold onto it after all of these years. Hope to update you all soon, peace.',
+                images: [
+                    'https://upload.wikimedia.org/wikipedia/commons/4/48/Alex_Jones_Portrait_%28cropped%29.jpg'
+                    ],
+            }
+        },
+		{
+            id: 0,
+            snippet: {
+                title: 'Huw Messie',
+                color: '#ff748c',
+                position: 'Designer',
+                positionDescription: 'Artist, Video Producer, Clothing Design',
+                description: 'Huw is a multimedia artist working in video/animation, game development and textiles. He’s produced several music videos in collaboration with LiveInColor and Charles Hussle, and works with LIC members on team projects. He also works experimentally in clothing and accessory design, using repurposed materials to create limited-edition pieces. Huw is a senior at Carnegie Mellon University studying fine arts and human-computer interaction.',
+                images: [
+                    'https://upload.wikimedia.org/wikipedia/commons/4/48/Alex_Jones_Portrait_%28cropped%29.jpg'
+                    ],
+            }
+        },
+		{
+            id: 0,
+            snippet: {
+                title: 'Greyson Flippo',
+                color: '#ff748c',
+                position: 'Designer',
+                positionDescription: 'Website Design, Programming, IT support',
+                description: 'Greyson is a Chicago-based website designer who is responsible for programming the LiveInColor website, and for assisting in the various technology that makes LiveInColor possible. He also is the designer of the Music Visualizer for Google Play™ Music, available for Google Chrome on the Chrome Web Store.',
+                images: [
+                    'https://upload.wikimedia.org/wikipedia/commons/4/48/Alex_Jones_Portrait_%28cropped%29.jpg'
+                    ],
+            }
+        },
     ]
 }
